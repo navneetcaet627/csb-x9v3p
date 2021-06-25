d7ec9464cbe35a60858fa7b04db256956fe573bb
Code Sandbox <hello@codesandbox.io>
2021-06-25 10:20:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d63770a1394f07dd9c4f32e0f78d95d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147172b9c9e5300ef6993c08b72f517a",
+    "url": "https://navneetcaet627.github.io/csb-x9v3p/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "https://navneetcaet627.github.io/csb-x9v3p/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "a3bf137c9dfeb7c4196f",
+    "url": "https://navneetcaet627.github.io/csb-x9v3p/static/js/main.27489dae.chunk.js"
+  },
+  {
+    "revision": "905c0f8c9d42955a3a6a",
+    "url": "https://navneetcaet627.github.io/csb-x9v3p/static/js/runtime-main.9f36eeb3.js"
+  }
+]);
